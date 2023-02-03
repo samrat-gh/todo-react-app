004ccf666a9b9db0c1c786d0a2d0a7ac6f3b6eae
greyhere <57859708+greyhere@users.noreply.github.com>
2023-02-03 19:22:10 +0530

    fix context

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,34 +1,61 @@
-import { useState, createContext} from "react"
+import { useContext } from 'react';
+import { themeContext } from '../App';
 
-export const themeContext = createContext('light');
+export default function Header({ name, toggleTheme }) {
+  const theme = useContext(themeContext);
 
-export default function Header({name}) {
+  return (
+    <header
+      className={
+        theme === 'light'
+          ? 'flex justify-between bg-gray-900 p-1'
+          : 'flex justify-between bg-gray-300 p-1'
+      }
+    >
+      <div>
+        <button>
+          {theme === 'light' ? (
+            <img
+              src={'darksettings.svg'}
+              alt='White setting icon'
+              width={24}
+              height={24}
+            />
+          ) : (
+            <img
+              src={'lightsettings.svg'}
+              alt='Dark setting icon'
+              width={24}
+              height={24}
+            />
+          )}
+        </button>
+      </div>
 
-    const [theme, setTheme] = useState('light');
+      <div
+        className={
+          theme === 'light'
+            ? 'text-white text-center font-bold'
+            : 'text-black text-center font-bold'
+        }
+      >
+        {name}
+      </div>
 
-return (
-    <themeContext.Provider value={theme}>
-     <header className={(theme === 'light')?"flex justify-between bg-gray-900 p-1"
-     : "flex justify-between bg-gray-300 p-1"}>
-        <div>
-            <button> 
-                {(theme === 'light') ? <img src={'darksettings.svg'} alt='White setting icon' width={24} height={24} />
-            : <img src={'lightsettings.svg'} alt='Dark setting icon' width={24} height={24} />}
-            </button>
-        </div>
-
-        <div className={(theme === 'light') ? 'text-white text-center font-bold' : 'text-black text-center font-bold'}>
-            {name}
-        </div>
-
-        <div>
-            <button onClick={() => setTheme(theme === 'light'? 'dark' : 'light')}>
-               {(theme ==='light') ? <img src={'lightmoon.svg'} alt='Light icon' width={24} height={24} />
-               : <img src={'darkmoon.svg'} alt='Dark icon' width={24} height={24} /> }
-            </button>
-        </div>
-     </header>
-     </themeContext.Provider>
-)
-};
-
+      <div>
+        <button onClick={toggleTheme}>
+          {theme === 'light' ? (
+            <img
+              src={'lightmoon.svg'}
+              alt='Light icon'
+              width={24}
+              height={24}
+            />
+          ) : (
+            <img src={'darkmoon.svg'} alt='Dark icon' width={24} height={24} />
+          )}
+        </button>
+      </div>
+    </header>
+  );
+}
